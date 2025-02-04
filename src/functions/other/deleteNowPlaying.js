47b2791f70738e04e1876d3af9fdb45293017a0a
Tyowk <tyowk@outlook.com>
2025-02-04 17:59:10 +0700

    Update deleteNowPlaying.js

--- src/functions/other/deleteNowPlaying.js
+++ src/functions/other/deleteNowPlaying.js
@@ -1,3 +1,5 @@
+const { setTimeout: Timeout } = require('timers/promises');
+
 /**
  * @param {import("..").Data} d
  */
@@ -6,7 +8,7 @@ module.exports = async d => {
 
     const { deleteNowPlaying } = d.client.music;
     if (deleteNowPlaying) return d.client.returnCode(d, data);
-    
+
     const manager = d.client.shoukaku;
     if (!manager) return d.aoiError.fnError(d, 'custom', {}, `Voice manager is not defined.`);
 
@@ -14,27 +16,29 @@ module.exports = async d => {
     if (!player || !player?.nowPlaying || !player?.nowPlaying?.channel || !player?.nowPlaying?.message)
         return d.client.returnCode(d, data);
 
-    const nowPlaying = player?.nowPlaying;
-    if (!nowPlaying || nowPlaying.isDeleted) return d.client.returnCode(d, data);
-    
+    let nowPlaying = player.nowPlaying;
+    if (!nowPlaying) return d.client.returnCode(d, data);
+    let maxAttempt = 20;
+
+    while (maxAttempt > 0 && nowPlaying.isDeleted) {
+        await Timeout(1000);
+        nowPlaying = player.nowPlaying;
+        maxAttempt--;
+    }
+
     try {
-        const channel =
-            d.client.channels.cache.get(nowPlaying?.channel) ||
-            (await d.client.channels.fetch(nowPlaying?.channel));
+        const channel = d.client.channels.cache.get(nowPlaying.channel) || await d.client.channels.fetch(nowPlaying.channel);
+        if (!channel) return d.client.returnCode(d, data);
 
-        const msg = channel
-            ? channel.messages.cache.get(nowPlaying?.message) ||
-              (await channel.messages.fetch(nowPlaying?.message))
-            : null;
-
-        if (!msg || !msg.deletable || msg.author.id !== d.client.user.id)
-            return d.client.returnCode(d, data);
+        const msg = channel.messages.cache.get(nowPlaying.message) || await channel.messages.fetch(nowPlaying.message);
+        if (!msg || !msg.deletable || msg.author.id !== d.client.user.id) return d.client.returnCode(d, data);
 
         await msg.delete();
-    } catch {}
+    } catch {};
 
-    if (!player.nowPlaying.isDeleted && player.nowPlaying.message === nowPlaying.message)
+    if (!player.nowPlaying.isDeleted && player.nowPlaying.message === nowPlaying.message) {
         player.nowPlaying.isDeleted = true;
+    }
 
     return {
         code: d.util.setCode(data)
