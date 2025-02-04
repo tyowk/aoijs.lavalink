33e461bb5b16cbb08ab096e5c70f3721b223cddb
Tyowk <tyowk@outlook.com>
2025-02-04 17:26:14 +0700

    Update deleteNowPlaying.js

--- src/functions/other/deleteNowPlaying.js
+++ src/functions/other/deleteNowPlaying.js
@@ -5,7 +5,7 @@ module.exports = async d => {
     const data = d.util.aoiFunc(d);
 
     const { deleteNowPlaying } = d.client.music;
-    if (deleteNowPlaying) return;
+    if (deleteNowPlaying) return d.client.returnCode(d, data);
     
     const manager = d.client.shoukaku;
     if (!manager) return d.aoiError.fnError(d, 'custom', {}, `Voice manager is not defined.`);
