84808e8946acd05ed985e73f8d42c5824d003819
Tyowk <tyowk@outlook.com>
2025-02-04 18:27:11 +0700

    Update deleteNowPlaying.js

--- src/functions/other/deleteNowPlaying.js
+++ src/functions/other/deleteNowPlaying.js
@@ -1,5 +1,3 @@
-const { setTimeout: Timeout } = require('timers/promises');
-
 /**
  * @param {import("..").Data} d
  */
@@ -13,33 +11,10 @@ module.exports = async d => {
     if (!manager) return d.aoiError.fnError(d, 'custom', {}, `Voice manager is not defined.`);
 
     const player = d.client.queue.get(d.guild.id);
-    if (!player || !player?.nowPlaying || !player?.nowPlaying?.channel || !player?.nowPlaying?.message)
-        return d.client.returnCode(d, data);
+    if (!player) return d.client.returnCode(d, data);
 
-    let nowPlaying = player.nowPlaying;
-    if (!nowPlaying) return d.client.returnCode(d, data);
-    let maxAttempt = 20;
-
-    while (maxAttempt > 0 && nowPlaying.isDeleted) {
-        await Timeout(1000);
-        nowPlaying = player.nowPlaying;
-        maxAttempt--;
-    }
-
-    try {
-        const channel = d.client.channels.cache.get(nowPlaying.channel) || await d.client.channels.fetch(nowPlaying.channel);
-        if (!channel) return d.client.returnCode(d, data);
-
-        const msg = channel.messages.cache.get(nowPlaying.message) || await channel.messages.fetch(nowPlaying.message);
-        if (!msg || !msg.deletable || msg.author.id !== d.client.user.id) return d.client.returnCode(d, data);
-
-        await msg.delete();
-    } catch {};
-
-    if (!player.nowPlaying.isDeleted && player.nowPlaying.message === nowPlaying.message) {
-        player.nowPlaying.isDeleted = true;
-    }
-
+    await player.deleteNowPlaying();
+    
     return {
         code: d.util.setCode(data)
     };
