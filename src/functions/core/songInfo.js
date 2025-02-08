69a738b00b1d878da4af23df5e084cd7b099c518
Tyowk <tyowk@outlook.com>
2025-02-08 11:16:25 +0700

    Update songInfo.js

--- src/functions/core/songInfo.js
+++ src/functions/core/songInfo.js
@@ -14,41 +14,43 @@ module.exports = d => {
 
     const getResult = res => {
         if (!res) return null;
-        const trackInfo = res;
-        const requester = trackInfo?.requester;
-        const pluginInfo = trackInfo?.plugininfo;
-
+        const trackInfo = track.info;
+        const requester = trackInfo.requester;
+        const pluginInfo = trackInfo.plugininfo;
+        const playlist = trackInfo.playlist;
+        
         const trackData = {
             title: trackInfo.title,
-            artworkUrl: trackInfo.artworkUrl,
-            artwork: trackInfo.artworkUrl,
             thumbnail: trackInfo.artworkUrl,
-            url: trackInfo.uri,
-            uri: trackInfo.uri,
+            url: trackInfo.url,
             duration: d.client.music.utils.formatTime(trackInfo.length),
             author: trackInfo.author,
-            sourceName: trackInfo.sourceName,
-            source: trackInfo.sourceName,
             platform: trackInfo.sourceName,
             identifier: trackInfo.identifier,
             isSeekable: trackInfo.isSeekable,
             isStream: trackInfo.isStream,
-            isrc: trackInfo.isrc || 'N/A',
-            durationMs: trackInfo.length || 'N/A',
-            queueLength: player.queue.length || 'N/A',
-            albumName: pluginInfo?.albumName,
-            albumUrl: pluginInfo?.albumUrl,
-            previewUrl: pluginInfo?.previewUrl,
-            isPreview: pluginInfo?.isPreview,
-            artist: trackInfo.author,
-            'artist.artworkUrl': pluginInfo?.artistArtworkUrl,
-            'artist.url': pluginInfo?.artistUrl,
+            isrc: trackInfo.isrc ?? null,
+            durationMs: trackInfo.length ?? 0,
+            queueLength: player.queue.length ?? 0,
+            historyLength: player.history.length ?? 0,
+            albumName: pluginInfo.albumName,
+            albumUrl: pluginInfo.albumUrl,
+            previewUrl: pluginInfo.previewUrl,
+            isPreview: pluginInfo.isPreview,
+            artist: trackInfo.artist,
+            'artist.avatar': pluginInfo.artistArtworkUrl,
+            'artist.url': pluginInfo.artistUrl,
             'requester.username': requester.username,
             'requester.globalName': requester.globalName,
             'requester.id': requester.id,
             'requester.avatar': requester.avatar,
             'requester.banner': requester.banner,
-            'requester.mention': requester.id ? `<@${requester.id}>` : null
+            'requester.mention': requester.id ? `<@${requester.id}>` : null,
+            'playlist.name': playlist.name,
+            'playlist.author': playlist.author,
+            'playlist.thumbnail': playlist.artworkUrl,
+            'playlist.tracks': playlist.totalTracks,
+            'playlist.url': playlist.url
         };
 
         return trackData[type];
