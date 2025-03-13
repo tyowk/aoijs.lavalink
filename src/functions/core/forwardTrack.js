5bfbeeed0d4f16a9804b7739cc145183880cb30a
Tyowk <tyowk.personal@gmail.com>
2025-03-13 22:47:52 +0700

    Add files via upload

---
+++ src/functions/core/forwardTrack.js
@@ -0,0 +1,27 @@
+/**
+ * @param {import("..").Data} d
+ */
+module.exports = (d) => {
+    const data = d.util.aoiFunc(d);
+    if (data.err) return d.error(data.err);
+    let [time = '10s'] = data.inside.splits;
+
+    const manager = d.client.shoukaku;
+    if (!manager) return d.aoiError.fnError(d, 'custom', {}, 'Voice manager is not defined.');
+
+    const player = d.client.queue.get(d.guild.id);
+    if (!player) return d.aoiError.fnError(d, 'custom', {}, 'There is no player for this guild.');
+
+    if (!player.current) return d.aoiError.fnError(d, 'custom', {}, 'There is no song currently playing.');
+    if (!player.current?.info?.isSeekable || player.current?.info?.isStream)
+        return d.aoiError.fnError(d, 'custom', {}, 'This current track cannot be forwarded.');
+    time = d.client.music.utils.parseTime(time?.addBrackets());
+    if (!time) return d.aoiError.fnError(d, 'custom', {}, 'Invalid time format.');
+
+    const currentTime = (player.player.position ?? 0) + 500;
+    player.seek(currentTime + time);
+
+    return {
+        code: d.util.setCode(data)
+    };
+};
