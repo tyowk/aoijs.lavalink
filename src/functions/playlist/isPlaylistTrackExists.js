683ab8378a0091cdc155ee649060634bb92706e0
Tyowk <tyowk@outlook.com>
2025-05-22 10:59:49 +0700

    add $isPlaylistTrackExists.js

---
+++ src/functions/playlist/isPlaylistTrackExists.js
@@ -0,0 +1,29 @@
+/**
+ * @param {import("..").Data} d
+ */
+module.exports = async (d) => {
+    const data = d.util.aoiFunc(d);
+    if (data.err) return d.error(data.err);
+  
+    let [name, index, id = d.author?.id] = data.inside.splits;
+    name = name?.addBrackets();
+    index = Number(index);
+
+    const manager = d.client.shoukaku;
+    if (!manager) return d.aoiError.fnError(d, 'custom', {}, 'Voice manager is not defined.');
+
+    const playlist = d.client.playlist || manager.playlist;
+    if (!playlist) return d.aoiError.fnError(d, 'custom', {}, 'Playlist manager is not defined.');
+    data.result = false;
+    
+    try {
+        const track = await playlist.getTrack(name, id, index);
+        data.result = track ? true : false;
+    } catch (err) {
+        data.result = false;
+    }
+    
+    return {
+        code: d.util.setCode(data)
+    };
+};
