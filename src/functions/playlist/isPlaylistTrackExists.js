776e0bc2db37473788fb578551cb03d5f5961688
Tyowk <tyowk.personal@gmail.com>
2025-05-24 06:23:29 +0000

    linter

--- src/functions/playlist/isPlaylistTrackExists.js
+++ src/functions/playlist/isPlaylistTrackExists.js
@@ -4,7 +4,7 @@
 module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
     if (data.err) return d.error(data.err);
-  
+
     let [name, index, id = d.author?.id] = data.inside.splits;
     name = name?.addBrackets();
     index = Number(index);
@@ -15,14 +15,14 @@ module.exports = async (d) => {
     const playlist = d.client.playlist || manager.playlist;
     if (!playlist) return d.aoiError.fnError(d, 'custom', {}, 'Playlist manager is not defined.');
     data.result = false;
-    
+
     try {
         const track = await playlist.getTrack(name, id, index);
-        data.result = track ? true : false;
+        data.result = !!track;
     } catch (err) {
         data.result = false;
     }
-    
+
     return {
         code: d.util.setCode(data)
     };
